6f35e0f73939759ace53f5ae56171749b2575528
Siyabonga Hadebe <92498693+Psyfo-C-YA@users.noreply.github.com>
2024-06-19 12:07:34 +0200

    Optimized for netlify deployment

--- sections/Landing.tsx
+++ sections/Landing.tsx
@@ -6,15 +6,28 @@
 // import { FaArrowRight } from "react-icons/fa6";
 
 // const Landing = () => {
+//   const scrollToContact = () => {
+//     console.log("Button clicked");
+//     const contactSection = document.getElementById("contact");
+//     console.log("Contact section:", contactSection);
+//     if (contactSection) {
+//       contactSection.scrollIntoView({ behavior: "smooth" });
+//     }
+//   };
+
 //   return (
 //     <div className="relative h-screen overflow-hidden p-8">
 //       {/* Header */}
 //       <Header />
 
 //       {/* Show Magnetic fancy button on sm screen and hide on md screen */}
-//       <div className="absolute bottom-36 left-10 z-20 md:hidden">
+//       <div className="absolute bottom-36 left-10 z-30 md:hidden">
 //         <MagneticWrapper>
-//           <FancyButton text="Contact Me" icon={<FaArrowRight />} />
+//           <FancyButton
+//             text="Contact Me"
+//             icon={<FaArrowRight />}
+//             onClick={scrollToContact}
+//           />
 //         </MagneticWrapper>
 //       </div>
 
@@ -24,7 +37,7 @@
 //       </div>
 
 //       {/* Slogan */}
-//       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mt-8 leading-[14vw] lg:leading-[10vw] 2xl:leading-[9rem] font-medium h-[40rem] tracking-[-0.3rem]">
+//       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mt-8 leading-[14vw] lg:leading-[10vw] 2xl:leading-[9rem] font-medium h-[40rem] tracking-[-0.3rem] ">
 //         <div className="flex flex-col justify-center gap-2 lg:gap-3 items-center text-primary-foreground text-[18vw] lg:text-[14vw] 2xl:text-[12rem] uppercase">
 //           <div>
 //             <span>Code</span>
@@ -47,7 +60,7 @@
 //         </div>
 
 //         {/*Magnetic Scroll Down  */}
-//         <MagneticWrapper className="absolute left-1/2 transform -translate-x-1/2 bottom-[8rem] md:bottom-[4rem] 2xl:-bottom-10">
+//         <MagneticWrapper className="absolute left-1/2 -translate-x-1/2 bottom-[8rem] md:bottom-[4rem] 2xl:-bottom-10">
 //           <ScrollDown />
 //         </MagneticWrapper>
 //       </div>
@@ -66,7 +79,9 @@
 
 // const Landing = () => {
 //   const scrollToContact = () => {
-//     const contactSection = document.querySelector("#contact");
+//     // console.log("Button clicked");
+//     const contactSection = document.getElementById("contact");
+//     // console.log("Contact section:", contactSection);
 //     if (contactSection) {
 //       contactSection.scrollIntoView({ behavior: "smooth" });
 //     }
@@ -78,9 +93,13 @@
 //       <Header />
 
 //       {/* Show Magnetic fancy button on sm screen and hide on md screen */}
-//       <div className="absolute bottom-36 left-10 z-20 md:hidden">
+//       <div className="absolute bottom-36 left-10 z-30 md:hidden">
 //         <MagneticWrapper>
-//           <FancyButton text="Contact Me" icon={<FaArrowRight />} onClick={scrollToContact} />
+//           <FancyButton
+//             text="Contact Me"
+//             icon={<FaArrowRight />}
+//             onClick={scrollToContact}
+//           />
 //         </MagneticWrapper>
 //       </div>
 
@@ -90,7 +109,7 @@
 //       </div>
 
 //       {/* Slogan */}
-//       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mt-8 leading-[14vw] lg:leading-[10vw] 2xl:leading-[9rem] font-medium h-[40rem] tracking-[-0.3rem]">
+//       <div className="absolute inset-0 flex flex-col justify-center items-center mt-8 leading-[14vw] lg:leading-[10vw] 2xl:leading-[9rem] font-medium tracking-[-0.3rem]">
 //         <div className="flex flex-col justify-center gap-2 lg:gap-3 items-center text-primary-foreground text-[18vw] lg:text-[14vw] 2xl:text-[12rem] uppercase">
 //           <div>
 //             <span>Code</span>
@@ -112,10 +131,77 @@
 //           </div>
 //         </div>
 
-//         {/*Magnetic Scroll Down  */}
-//         <MagneticWrapper className="absolute left-1/2 transform -translate-x-1/2 bottom-[8rem] md:bottom-[4rem] 2xl:-bottom-10">
+//         {/* Magnetic Scroll Down */}
+//         <MagneticWrapper className="absolute left-1/2 -translate-x-1/2 bottom-[8rem] md:bottom-[4rem] 2xl:-bottom-10">
 //           <ScrollDown />
 //         </MagneticWrapper>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Landing;
+
+// import Header from "@/components/navigation/header/Header";
+// import FancyButton from "@/components/ui/FancyButton";
+// import LiveClock from "@/components/ui/LiveClock";
+// import ScrollDown from "@/components/ui/ScrollDown";
+// import MagneticWrapper from "@/components/visualEffects/MagneticWrapper";
+// import { FaArrowRight } from "react-icons/fa6";
+
+// const Landing = () => {
+//   const scrollToContact = () => {
+//     const contactSection = document.getElementById("contact");
+//     if (contactSection) {
+//       contactSection.scrollIntoView({ behavior: "smooth" });
+//     }
+//   };
+
+//   return (
+//     <div className="relative h-screen overflow-hidden p-8">
+//       {/* Header */}
+//       <Header />
+
+//       {/* Centered elements on small devices */}
+//       <div className="absolute bottom-36 left-1/2 transform -translate-x-1/2 z-30 flex flex-col items-center gap-4 md:hidden">
+//         <MagneticWrapper>
+//           <FancyButton
+//             text="Contact Me"
+//             icon={<FaArrowRight />}
+//             onClick={scrollToContact}
+//           />
+//         </MagneticWrapper>
+//         <ScrollDown />
+//         <LiveClock timeZone="Johannesburg" />
+//       </div>
+
+//       {/* Live Clock for larger devices */}
+//       <div className="hidden md:block absolute right-10 bottom-10">
+//         <LiveClock timeZone="Johannesburg" />
+//       </div>
+
+//       {/* Slogan */}
+//       <div className="absolute inset-0 flex flex-col justify-center items-center mt-8 leading-[14vw] lg:leading-[10vw] 2xl:leading-[9rem] font-medium tracking-[-0.3rem]">
+//         <div className="flex flex-col justify-center gap-2 lg:gap-3 items-center text-primary-foreground text-[18vw] lg:text-[14vw] 2xl:text-[12rem] uppercase">
+//           <div>
+//             <span>Code</span>
+//           </div>
+//           <div>
+//             <span>Digital </span>
+//           </div>
+//           <div className="relative">
+//             <span>Wonders </span>
+//             <div className="text-[1rem] leading-[1.4rem] tracking-[-0.07rem] absolute top-[14vw] lg:top-[10vw] 2xl:top-[9rem] left-0 2xl:left-[50rem] w-[30rem] uppercase font-normal">
+//               <span>Code magician on a mission</span>
+//               <br />
+//               <span>creating digital wonders</span>
+//               <br />
+//               <span>through elegant code</span>
+//               <br />
+//               <span>bringing passion to development</span>
+//             </div>
+//           </div>
+//         </div>
 //       </div>
 //     </div>
 //   );
@@ -132,9 +218,7 @@ import { FaArrowRight } from "react-icons/fa6";
 
 const Landing = () => {
   const scrollToContact = () => {
-    console.log("Button clicked");
     const contactSection = document.getElementById("contact");
-    console.log("Contact section:", contactSection);
     if (contactSection) {
       contactSection.scrollIntoView({ behavior: "smooth" });
     }
@@ -145,24 +229,8 @@ const Landing = () => {
       {/* Header */}
       <Header />
 
-      {/* Show Magnetic fancy button on sm screen and hide on md screen */}
-      <div className="absolute bottom-36 left-10 z-30 md:hidden">
-        <MagneticWrapper>
-          <FancyButton
-            text="Contact Me"
-            icon={<FaArrowRight />}
-            onClick={scrollToContact}
-          />
-        </MagneticWrapper>
-      </div>
-
-      {/* Live Clock */}
-      <div className="absolute right-10 bottom-10">
-        <LiveClock timeZone="Johannesburg" />
-      </div>
-
       {/* Slogan */}
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mt-8 leading-[14vw] lg:leading-[10vw] 2xl:leading-[9rem] font-medium h-[40rem] tracking-[-0.3rem]">
+      <div className="relative inset-0 flex flex-col justify-center items-center mt-8 leading-[14vw] lg:leading-[10vw] 2xl:leading-[9rem] font-medium tracking-[-0.3rem]">
         <div className="flex flex-col justify-center gap-2 lg:gap-3 items-center text-primary-foreground text-[18vw] lg:text-[14vw] 2xl:text-[12rem] uppercase">
           <div>
             <span>Code</span>
@@ -183,11 +251,24 @@ const Landing = () => {
             </div>
           </div>
         </div>
+      </div>
 
-        {/*Magnetic Scroll Down  */}
-        <MagneticWrapper className="absolute left-1/2 transform -translate-x-1/2 bottom-[8rem] md:bottom-[4rem] 2xl:-bottom-10">
-          <ScrollDown />
+      {/* Centered elements on small devices after the slogan */}
+      <div className="relative -bottom-28 left-1/2 transform -translate-x-1/2 z-30 flex flex-col items-center gap-4 md:hidden">
+        <MagneticWrapper>
+          <FancyButton
+            text="Contact Me"
+            icon={<FaArrowRight />}
+            onClick={scrollToContact}
+          />
         </MagneticWrapper>
+        {/* <ScrollDown /> */}
+        <LiveClock timeZone="Johannesburg" />
+      </div>
+
+      {/* Live Clock for larger devices */}
+      <div className="hidden md:block absolute right-10 bottom-10">
+        <LiveClock timeZone="Johannesburg" />
       </div>
     </div>
   );
